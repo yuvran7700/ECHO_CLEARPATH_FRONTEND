2f6641e61744dd0552ce78fad60433197bb4d6e4
Serene <s.lobo@student.unsw.edu.au>
2026-04-16 00:03:08 +1000

    Updated app.jsx for routing, created stub pages

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+
+const LandingPage = () => {
+    return (
+        <div>
+            <section className="hero-section">
+                <h1>This is a landing page</h1>
+                <p>Landing wow</p>
+                <button className="cta-main">Explore Dashboards</button>
+            </section>
+        </div>
+    );
+};
+
+export default LandingPage;
