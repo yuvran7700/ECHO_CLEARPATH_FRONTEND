f40433a8ee821f88b846563bdd2f6aa26f3d04b7
Serene <s.lobo@student.unsw.edu.au>
2026-04-22 22:13:13 +1000

    chore: deleted dead code

--- src/components/AccordianObject.jsx
+++ src/components/AccordianObject.jsx
@@ -90,7 +90,6 @@ const AccordionObject = ({ items }) => {
 
 export default AccordionObject
 
-// import Accordion from '@mui/material/Accordion';
 // import AccordionSummary from '@mui/material/AccordionSummary';
 // import AccordionDetails from '@mui/material/AccordionDetails';
 // import Typography from '@mui/material/Typography';
